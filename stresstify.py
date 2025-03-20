5e6ca86cbbe28771f7b4b53e629aec2e4c274b80
Pinkysha228 <oleksijborsulak@gmail.com>
2025-03-20 19:04:21 +0200

    Update stresstify.py

--- stresstify.py
+++ stresstify.py
@@ -67,28 +67,32 @@ def disk_info():
     return disk_info_dict
 
 
-def cpu_calculation(number_a, i, size=10000000):
-    try:
-        start_time = time.time()
-        result = number_a ** size
-        elapsed_time = round(time.time() - start_time, 2)
-        cpu_load = psutil.cpu_percent(interval=1)
-        cpu_freq = psutil.cpu_freq().current
+def cpu_calculation(i, size=5000, max_retries=3):
+    for _ in range(max_retries):
+        try:
+            start_time = time.time()
+            a = np.random.rand(size, size)
+            b = np.random.rand(size, size)
+            result = np.dot(a, b)
+            elapsed_time = round(time.time() - start_time, 2)
 
-        del result
-        gc.collect()
+            cpu_load = psutil.cpu_percent(interval=None)
+            cpu_freq = psutil.cpu_freq().current
 
-        return i, elapsed_time, cpu_load, cpu_freq
-    except MemoryError:
-        return i, None, None, None
+            del result
+            gc.collect()
+
+            return i, elapsed_time, cpu_load, cpu_freq
+        except MemoryError:
+            time.sleep(0.5)
+            continue
+    return i, None, None, None
+
 
 
 class StressTest:
-    def __init__(self, debug=False):
-        self.debug = debug
-
-    def ram_test(self, size=8):
-        num_elements = size * 1024 * 1024 // 4  # 8 MB
+    def ram_test(self, size=8, debug=False):
+        num_elements = size * 1024 * 1024 // 4
         elements = []
         start_time = time.time()
         ram_used = {}
@@ -97,71 +101,67 @@ class StressTest:
                 memory = np.ones(num_elements, dtype=np.float32)
                 elements.append(memory)
 
-                if self.debug:
+                if debug:
                     used_percent = psutil.virtual_memory().percent
                     ram_used[round(time.time() - start_time, 2)] = f'{used_percent:.2f}%'
             except MemoryError:
                 elements.clear()
                 break
 
-        if self.debug:
+        if debug:
             elapsed_time = round(time.time() - start_time)
             full_dict = {'elapsed_time': elapsed_time,
                          'ram_used': ram_used}
             return full_dict
 
-    def memory_test(self):
+    def memory_test(self, debug=False, size=128*1024**2):
         filename = 'testfile.bin'
-        file_size = 1 * 1024 * 1024 * 1024  # 1 ГБ
 
         write_time_dict = {}
         read_time_dict = {}
         write_dict = {}
         read_dict = {}
 
-        data = os.urandom(file_size)  # сразу создаём 1 ГБ в памяти
+        data = os.urandom(size)
 
         for i in range(1, 11):
-            # замеряем время записи
             start_time = time.time()
             with open(filename, 'wb') as file:
                 file.write(data)
             elapsed_time = round(time.time() - start_time, 2)
 
-            # замеряем время чтения
             start_time = time.time()
             with open(filename, 'rb') as file:
                 data = file.read()
+            os.remove(filename)
             elapsed_time2 = round(time.time() - start_time, 2)
 
-            write_speed = round((file_size / (1024 * 1024)) / elapsed_time, 2)  # MB/s
-            read_speed = round((file_size / (1024 * 1024)) / elapsed_time2, 2)  # MB/s
+            write_speed_mb = round((size / (1024 * 1024)) / elapsed_time, 2)  # MB/s
+            read_speed_mb = round((size / (1024 * 1024)) / elapsed_time2, 2)  # MB/s
 
-            if self.debug:
+            if debug:
                 write_time_dict[i] = elapsed_time
                 read_time_dict[i] = elapsed_time2
-                write_dict[i] = write_speed
-                read_dict[i] = read_speed
+                write_dict[i] = write_speed_mb
+                read_dict[i] = read_speed_mb
             else:
-                return write_speed, read_speed, elapsed_time
+                return write_speed_mb, read_speed_mb, elapsed_time
 
-        if self.debug:
+        if debug:
             average_write_speed = round(sum(write_dict.values()) / len(write_dict), 2)
             write_average_time = round(sum(write_time_dict.values()) / len(write_time_dict), 2)
             average_read_speed = round(sum(read_dict.values()) / len(read_dict), 2)
             read_average_time = round(sum(read_time_dict.values()) / len(read_time_dict), 2)
 
             full_dict = {
-                'average_write_speed': average_write_speed,
-                'average_write_time': write_average_time,
-                'average_read_speed': average_read_speed,
-                'read_average_time': read_average_time
+                'average_write_speed_mb': average_write_speed,
+                'average_write_time_s': write_average_time,
+                'average_read_speed_mb': average_read_speed,
+                'read_average_time_s': read_average_time
             }
             return full_dict
 
-    def cpu_test(self, size=10000000, iterations=11):
-        number_a = 2 ** 256
-
+    def cpu_test(self, size=11585, iterations=11, debug=False):
         start_cpu_load = psutil.cpu_percent(interval=1)
         start_cpu_freq = psutil.cpu_freq().current
 
@@ -170,17 +170,19 @@ class StressTest:
         time_dict = {}
 
         with multiprocessing.Pool(processes=multiprocessing.cpu_count()) as pool:
-            results = pool.starmap(cpu_calculation, [(number_a, i, size) for i in range(1, iterations)])
+            results = pool.starmap(cpu_calculation, [(i, size) for i in range(1, iterations + 1)])
 
-        if self.debug:
-            for i, elapsed_time, cpu_load, cpu_freq in results:
-                if cpu_load is not None:
-                    cpu_load_dict[i] = cpu_load
-                if cpu_freq is not None:
-                    cpu_freq_dict[i] = cpu_freq
-                if elapsed_time is not None:
-                    time_dict[i] = elapsed_time
+        for i, elapsed_time, cpu_load, cpu_freq in results:
+            if cpu_load is None or cpu_freq is None:
+                i, elapsed_time, cpu_load, cpu_freq = cpu_calculation(i, size)
+            if cpu_load is not None:
+                cpu_load_dict[i] = cpu_load
+            if cpu_freq is not None:
+                cpu_freq_dict[i] = cpu_freq
+            if elapsed_time is not None:
+                time_dict[i] = elapsed_time
 
+        if debug:
             valid_cpu_loads = [load for load in cpu_load_dict.values() if load is not None]
             valid_cpu_freqs = [freq for freq in cpu_freq_dict.values() if freq is not None]
             valid_times = [time for time in time_dict.values() if time is not None]
@@ -200,4 +202,4 @@ class StressTest:
 
             return cpu_full_dict
         else:
-            return results[0][1]
+            return results[0][1] if results and results[0][1] is not None else 0
